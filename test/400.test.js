8dfe1148cc05184bcc9e7dd97d47db4d17c43a6d
William Killerud <william.killerud@schibsted.com>
2024-04-04 14:13:23 +0200

    test: add test case for 400 bad request
    Fails until we get an updated core

---
+++ test/400.test.js
@@ -0,0 +1,78 @@
+import FormData from 'form-data';
+import Fastify from 'fastify';
+import fetch from 'node-fetch';
+import path from 'path';
+import tap from 'tap';
+import url from 'url';
+import fs from 'fs';
+
+import Sink from '@eik/core/lib/sinks/test.js';
+import Server from '../lib/main.js';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const FIXTURE_PKG = path.resolve(__dirname, '../fixtures/archive.tgz');
+
+tap.test('400 - GET request with non-existing hostname', async (t) => {
+    const sink = new Sink();
+    const service = new Server({ customSink: sink });
+
+    const app = Fastify({
+        ignoreTrailingSlash: true,
+    });
+    app.register(service.api());
+
+    const address = await app.listen({ port: 0, host: '127.0.0.1' });
+
+    let formData = new FormData();
+    formData.append('key', 'change_me');
+
+    const res = await fetch(`${address}/auth/login`, {
+        method: 'POST',
+        body: formData,
+        headers: formData.getHeaders(),
+    });
+
+    const { token } = await res.json();
+
+    formData = new FormData();
+    formData.append('package', fs.createReadStream(FIXTURE_PKG));
+
+    // PUT files on server so we don't get 404
+    const uploaded = await fetch(`${address}/pkg/@cuz/fuzz/1.4.8`, {
+        method: 'PUT',
+        body: formData,
+        redirect: 'manual',
+        headers: { ...formData.getHeaders(), Authorization: `Bearer ${token}` },
+    });
+
+    t.equal(
+        uploaded.status,
+        303,
+        'on PUT of package, server should respond with a 303 redirect',
+    );
+    t.equal(
+        uploaded.headers.get('location'),
+        `/pkg/@cuz/fuzz/1.4.8`,
+        'on PUT of package, server should respond with a location header',
+    );
+
+    // GET file from server
+    const response = await fetch(
+        `${address}/pkg/@cuz/fuzz/1.4.8/main/index.js`,
+        {
+            method: 'GET',
+            headers: {
+                Host: 'leethaxorz.ai',
+            },
+        },
+    );
+
+    t.equal(
+        response.status,
+        400,
+        'server should respond with a 400 Bad Request',
+    );
+
+    await app.close();
+});
